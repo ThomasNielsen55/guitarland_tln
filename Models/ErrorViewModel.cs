1b58af1068c2c46b6f0d746cf96bb4319916ecd2
Thomas Nielsen <94662330+ThomasNielsen55@users.noreply.github.com>
2024-02-09 11:30:20 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace guitarland_tln.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
